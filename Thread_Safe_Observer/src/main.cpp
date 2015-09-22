b979ee8ac45c5bdfa2b2cb64fa829a2d22c10786
Kingsley Chen <kingsamchen@gmail.com>
2015-09-22 16:58:14 +0800

    Update usage sample.

--- Thread_Safe_Observer/src/main.cpp
+++ Thread_Safe_Observer/src/main.cpp
@@ -3,18 +3,63 @@
 */
 
 #include <iostream>
+#include <mutex>
 
 #include "observer_list.h"
 
-class Subject {
+namespace {
+
+class Widget {
+public:
+    class Observer : public std::enable_shared_from_this<Observer> {
+    public:
+        virtual ~Observer() = default;
+        virtual void PositionChanged(int x, int y) = 0;
+    };
+
+    void AddObserver(const std::weak_ptr<Observer>& observer)
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        observers_.Add(observer);
+    }
+
+    void RemoveObserver(const std::weak_ptr<Observer>& observer)
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        observers_.Remove(observer);
+    }
+
+    void NotifyFoo(int x, int y)
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        FOR_EACH_OBSERVER(Observer, observers_, PositionChanged(x, y));
+    }
+
+private:
+    ObserverList<Observer> observers_;
+    std::mutex mutex_;
 };
+
+class PositionWatcher : public Widget::Observer {
+public:
+    void PositionChanged(int x, int y) override
+    {
+        std::cout << "new position (" << x << ", " << y << ")" << std::endl;
+    }
+};
+
+}
 
 int main()
 {
-    ObserverList<Subject> observer_list;
-    observer_list.Add(std::make_shared<Subject>());
-    observer_list.Remove(std::make_shared<Subject>());
-    std::cout << "hello world" << std::endl;
+    Widget my_widget;
+    {
+        auto watcher = std::make_shared<PositionWatcher>();
+        my_widget.AddObserver(watcher);
+        my_widget.NotifyFoo(20, 10);
+    }
+    my_widget.NotifyFoo(10, 20);
+
     return 0;
 }
 
