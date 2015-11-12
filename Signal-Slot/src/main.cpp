0e69201ed1996e2823eec0c8ca660e21b1722c8a
kingsleychen <kingsamchen@gmail.com>
2015-11-13 02:24:10 +0800

    Signal-Slot: update test code

--- Signal-Slot/src/main.cpp
+++ Signal-Slot/src/main.cpp
@@ -3,6 +3,7 @@
 */
 
 #include <iostream>
+#include <functional>
 
 #include "signals.h"
 
@@ -12,22 +13,33 @@ void foo(int i, const int& j, int&& k)
 }
 
 struct Foo {
-    static void foo(int i, const int& j, int&& k)
+    static void sfoo(int i, const int& j, int&& k)
     {
+        std::cout << "Foo::sfoo" << std::endl;
+    }
+
+    void foo(int i, const int& j, int&& k)
+    {
+        s = "world";
         std::cout << "Foo::foo" << std::endl;
     }
+
+    std::string s = "hello";
 };
 
 int main()
 {
     Signal<int, const int&, int&&> s;
-    std::function<void(int, const int&, int&&)> f(&foo);
     Slot slot = s.Connect(&foo);
-    s.Connect(&Foo::foo);
-    s.Connect(f);
+    s.Connect(&Foo::sfoo);
+    auto foo = std::make_shared<Foo>();
+    auto pf = foo.get();
+    using namespace std::placeholders;
+    s.Connect(std::bind(&Foo::foo, pf, _1, _2, _3), foo);
     s.Emit(1, 3, 5);
-
     slot.Disconnect();
+    s.Emit(5, 3, 1);
+    foo.reset();
     s.Emit(5, 3, 1);
     s.DisconnectAll();
     s.Emit(3, 2, 1);
