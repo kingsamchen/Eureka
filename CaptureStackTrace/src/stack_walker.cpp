3679715326cd6e32b155c144e88f9a6669b4b014
kingsleychen <kingsamchen@gmail.com>
2016-01-09 15:45:21 +0800

    CaptureStackTrace: add the feature capturing call-stack on calling(current) thread.

--- CaptureStackTrace/src/stack_walker.cpp
+++ CaptureStackTrace/src/stack_walker.cpp
@@ -3,3 +3,21 @@
 */
 
 #include "stack_walker.h"
+
+#include <sstream>
+
+#include <Windows.h>
+
+// Prevent stack frames getting messed by FPO.
+#pragma optimize("", off)
+
+StackWalker::StackWalker()
+{
+    auto captured_frame_count = RtlCaptureStackBackTrace(0,
+                                                         static_cast<DWORD>(stack_frames_.size()),
+                                                         stack_frames_.data(),
+                                                         nullptr);
+    valid_frame_count_ = captured_frame_count;
+}
+
+#pragma optimize("", on)
--- CaptureStackTrace/src/stack_walker.h
+++ CaptureStackTrace/src/stack_walker.h
@@ -9,4 +9,25 @@
 #ifndef CAPTURE_STACK_TRACE_STACK_WALKER_H_
 #define CAPTURE_STACK_TRACE_STACK_WALKER_H_
 
+#include <array>
+
+#include "basic_macros.h"
+
+class StackWalker {
+public:
+    // Captures stack frame on calling thread.
+    StackWalker();
+
+    ~StackWalker() = default;
+
+    DEFAULT_COPY(StackWalker);
+
+    DEFAULT_MOVE(StackWalker);
+
+private:
+    static const size_t kMaxStackFrames = 64;
+    std::array<void*, kMaxStackFrames> stack_frames_;
+    int valid_frame_count_ = 0;
+};
+
 #endif  // CAPTURE_STACK_TRACE_STACK_WALKER_H_
