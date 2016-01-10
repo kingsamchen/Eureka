ad036325377799c182fe6d032dc9a47f22d14831
kingsleychen <kingsamchen@gmail.com>
2016-01-11 00:50:20 +0800

    CaptureCallStack: add quick test code.

--- CaptureStackTrace/src/main.cpp
+++ CaptureStackTrace/src/main.cpp
@@ -2,7 +2,41 @@
  @ 0xCCCCCCCC
 */
 
+#include <conio.h>
+
+#include <iostream>
+
+#include "stack_walker.h"
+
+void CaptureCallStack()
+{
+    StackWalker callstack;
+    callstack.OutputCallStack(std::cout);
+}
+
+void Func3(int& val)
+{
+    ++val;
+    std::cout << val;
+    CaptureCallStack();
+}
+
+void Func2(int& val)
+{
+    ++val;
+    Func3(val);
+}
+
+void Func1(int& val)
+{
+    ++val;
+    Func2(val);
+}
+
 int main()
 {
+    int i = 0;
+    Func1(i);
+    _getch();
     return 0;
 }
