931a129b6b68483824694416ec2c642a1eea7be4
Kingsley Chen <kingsamchen@gmail.com>
2016-01-14 19:49:43 +0800

    Main function prototype

--- MiniDumper/src/minidumper.h
+++ MiniDumper/src/minidumper.h
@@ -9,4 +9,10 @@
 #ifndef MINIDUMPER_MINIDUMPER_H_
 #define MINIDUMPER_MINIDUMPER_H_
 
+#include <string>
+
+#include <Windows.h>
+
+void CreateMiniDump(const std::wstring& path, EXCEPTION_POINTERS* exception_pointers);
+
 #endif  // MINIDUMPER_MINIDUMPER_H_
