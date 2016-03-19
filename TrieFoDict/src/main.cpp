818a202d5edebec06787698a67b7ac24368e78d6
Kingsley Chen <kingsamchen@gmail.com>
2016-03-19 18:57:23 +0800

    TrieFoDict updated: add quick-and-dirty test.

--- TrieFoDict/src/main.cpp
+++ TrieFoDict/src/main.cpp
@@ -4,10 +4,36 @@
 
 #include <iostream>
 
+#include <cassert>
+
+#include "alphabet.h"
+#include "basic_macros.h"
 #include "trie.h"
+
+void AssertAlphabet()
+{
+    assert(Alphabet::length() == 27);
+
+    assert(Alphabet::GetCharacterIndex('a') == 0);
+    assert(Alphabet::GetCharacterIndex('-') == 26);
+    assert(Alphabet::GetCharacterIndex(' ') == Alphabet::npos);
+}
+
+void AssertTrieCtorDtor()
+{
+    Trie trie;
+    UNREFED_VAR(trie);
+}
+
+void AssertTrieInsert()
+{
+    Trie trie;
+    trie.Insert("hello");
+    //trie.Insert("go ahead");
+}
 
 int main()
 {
-    Trie trie;
+    AssertTrieInsert();
     return 0;
 }
