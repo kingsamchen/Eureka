7df4a3838043d397da7d09910f19dc2ed2814c75
Kingsley Chen <kingsamchen@gmail.com>
2016-03-22 19:29:05 +0800

    TernaryTree: add insert.

--- TernarySearchTree/src/main.cpp
+++ TernarySearchTree/src/main.cpp
@@ -2,6 +2,16 @@
  @ 0xCCCCCCCC
 */
 
+#include "ternary_search_tree.h"
+
+void TestInsertion()
+{
+    TernaryTree ternary_tree;
+    ternary_tree.Insert("hello");
+    ternary_tree.Insert("here");
+    ternary_tree.Insert("world");
+}
+
 int main()
 {
     return 0;
--- TernarySearchTree/src/ternary_search_tree.cpp
+++ TernarySearchTree/src/ternary_search_tree.cpp
@@ -4,19 +4,30 @@
 
 #include "ternary_search_tree.h"
 
+#include <cassert>
+
 struct Node {
+    static constexpr size_t kLeft = 0;
+    static constexpr size_t kMiddle = 1;
+    static constexpr size_t kRight = 2;
+
     char character { 0 };
     bool end_word { false };
-    Node* left_child { nullptr };
-    Node* middle_child { nullptr };
-    Node* right_child { nullptr };
+    Node* children[3] { nullptr };
 };
 
 namespace {
 
-void DestroyTree(Node* root) noexcept
+void DestroyTree(Node* node) noexcept
 {
-    // TODO:
+    assert(node != nullptr);
+    for (auto child : node->children) {
+        if (child) {
+            DestroyTree(child);
+        }
+    }
+
+    delete node;
 }
 
 }   // namespace
@@ -30,5 +41,12 @@ TernaryTree::~TernaryTree()
     if (root_) {
         DestroyTree(root_);
         root_ = nullptr;
+    }
+}
+
+void TernaryTree::Insert(const std::string& word)
+{
+    for (char ch : word) {
+
     }
 }
