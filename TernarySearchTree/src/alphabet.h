a50eb96abf65c567a7ba419bd4d32ccecbf5ec72
Kingsley Chen <kingsamchen@gmail.com>
2016-03-22 17:38:57 +0800

    TernarySearchTree: add auxiliary files.

---
+++ TernarySearchTree/src/alphabet.h
@@ -0,0 +1,25 @@
+/*
+ @ 0xCCCCCCCC
+*/
+
+#if defined(_MSC_VER)
+#pragma once
+#endif
+
+#ifndef TRIE_FOR_DICT_ALPHABET_H_
+#define TRIE_FOR_DICT_ALPHABET_H_
+
+class Alphabet {
+public:
+    Alphabet() = delete;
+
+    ~Alphabet() = delete;
+
+    static size_t GetCharacterIndex(char ch);
+
+    static size_t length();
+
+    static size_t npos;
+};
+
+#endif  // TRIE_FOR_DICT_ALPHABET_H_
