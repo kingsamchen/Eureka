906140cc4e81647c59cbe625231baac22ca439fd
KingsleyChen <kingsamchen@gmail.com>
2016-04-15 17:47:56 +0800

    splash screen for android

---
+++ AndroidSplashScreen/app/src/main/java/com/kc/example/androidsplashscreen/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.kc.example.androidsplashscreen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        final int duration = 2000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, duration);
+    }
+}
