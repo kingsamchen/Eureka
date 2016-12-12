dba274cf76ed3fe16d193bc2b084467d55478781
Kingsley Chen <kingsamchen@gmail.com>
2016-12-12 11:12:08 +0800

    SimpleDownloader: using a dedicated worker thread as downloader consumer
    Also disable warning C4819

--- SimpleDownloader/src/main.cpp
+++ SimpleDownloader/src/main.cpp
@@ -6,6 +6,7 @@
 
 #include "base/at_exit.h"
 #include "base/command_line.h"
+#include "base/threading/thread.h"
 
 #include "downloader/url_downloader.h"
 
@@ -27,20 +28,32 @@ public:
     }
 };
 
+GURL url("http://photo.fanfou.com/v1/mss_3d027b52ec5a4d589e68050845611e68/ff/n0/0d/ab/yr_164422.jpg@596w_1l.jpg");
+base::FilePath path(L"D:\\misc\\test.jpg");
+DownloadCompleteEvent complete_event;
+std::unique_ptr<bililive::URLDownloader> downloader;
+
 int main(int argc, char* argv[])
 {
     CommandLine::Init(0, nullptr);
     base::AtExitManager exit_manager;
 
-    base::MessageLoopForIO loop;
+    base::Thread worker_thread("worker");
+    base::Thread::Options thread_options(base::MessageLoop::TYPE_IO, 0);
+    DCHECK(worker_thread.StartWithOptions(thread_options));
 
-    GURL url("http://photo.fanfou.com/v1/mss_3d027b52ec5a4d589e68050845611e68/ff/n0/0d/ab/yr_164422.jpg@596w_1l.jpg");
-    base::FilePath path(L"D:\\misc\\test.jpg");
-    DownloadCompleteEvent complete_event;
-    bililive::URLDownloader downloader(url, path, &complete_event);
-    downloader.Start();
+    using thread_task_fn = void(*)();
 
-    loop.Run();
+    // Seems URLRequestContext requires construction and starting request must be on the same thread.
+    thread_task_fn run_download = []() {
+        downloader = std::make_unique<bililive::URLDownloader>(url, path, &complete_event);
+        downloader->Start();
+    };
+
+    worker_thread.message_loop_proxy()->PostTask(FROM_HERE, base::Bind(run_download));
+
+    std::cin.get();
+    worker_thread.Stop();
 
     return 0;
 }
