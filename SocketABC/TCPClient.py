a03293e737ccc23c26f4daf447e47b49a5a669b5
Kingsley Chen <kingsamchen@live.com>
2018-02-09 01:19:25 +0800

    Modify tcpclient tool

--- SocketABC/TCPClient.py
+++ SocketABC/TCPClient.py
@@ -4,22 +4,25 @@ __author__ = '0xCCCCCCCC'
 from socket import *
 
 SERVER_NAME = '127.0.0.1'
-SERVER_PORT = 12345
+SERVER_PORT = 8088
 
 
 def main():
-    buffer_size = 2048
-    while True:
-        message = raw_input('Please input a message:')
-        if message == 'exit':
-            break
-        client_socket = socket(AF_INET, SOCK_STREAM)
-        client_socket.connect((SERVER_NAME, SERVER_PORT))
-        client_socket.send(message)
-        modified_message = client_socket.recv(buffer_size)
-        print('received reply: ' + modified_message)
-        client_socket.close()
+    #buffer_size = 2048
 
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.connect((SERVER_NAME, SERVER_PORT))
+
+    while True:
+        message = input('Please input a message:')
+        if message == 'exit':
+            client_socket.close()
+            break
+
+        count = client_socket.send(message.encode('utf-8'))
+        print('data in {} bytes sent'.format(count))
+        #modified_message = client_socket.recv(buffer_size)
+        #print('received reply: ' + modified_message)
 
 if __name__ == '__main__':
     main()
