85179b37b7e66f7edbc5c7178746f257905f457b
Kingsley Chen <kingsamchen@gmail.com>
2018-10-10 19:40:48 +0800

    Add single-thread and multi-thread test cases for LStack

--- LockfreeStack/main.cpp
+++ LockfreeStack/main.cpp
@@ -1,19 +1,71 @@
 
+#include <algorithm>
+#include <cassert>
 #include <iostream>
+#include <thread>
+#include <vector>
+#include <random>
 
 #include "lstack.h"
 
-int main()
+void SingleThread()
 {
     LStack<int> ls(5);
+
     ls.Push(1);
     ls.Push(3);
     ls.Push(5);
     ls.Pop();
     ls.Pop();
     ls.Pop();
-    auto rv = ls.Pop();
-    std::cout << rv.first;
+
+    assert(!ls.Pop().first);
+}
+
+void Multithread()
+{
+    LStack<int> ls(100);
+
+    std::thread producer([&ls] {
+        std::random_device rd;
+        std::default_random_engine e(rd());
+        std::uniform_int_distribution<int> dist(0, 1000);
+
+        for (int i = 0; i < 50; ++i) {
+            auto t = dist(e);
+            auto ret = ls.Push(t);
+            if (ret) {
+                printf("P -> %d\n", t);
+            }
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(t));
+        }
+    });
+
+    std::thread consumer([&ls] {
+        std::random_device rd;
+        std::default_random_engine e(rd());
+        std::uniform_int_distribution<int> dist(0, 1000);
+
+        for (int i = 0; i < 50; ++i) {
+            auto t = dist(e);
+            auto r = ls.Pop();
+            if (r.first) {
+                printf("C <- %d\n", r.second);
+            }
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(t));
+        }
+    });
+
+    producer.join();
+    consumer.join();
+}
+
+int main()
+{
+    // SingleThread();
+    Multithread();
     return 0;
 }
 
