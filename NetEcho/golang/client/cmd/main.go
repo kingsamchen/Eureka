17b281b6ed8d785b49fda3aad5ac40231aaad011
Kingsley Chen <kingsamchen@gmail.com>
2018-12-25 15:20:46 +0800

    Add echo-client implementation in golang

--- NetEcho/golang/client/cmd/main.go
+++ NetEcho/golang/client/cmd/main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func serve(conn net.Conn) {
+
+}
 
 func main() {
-	fmt.Println("echo client")
+	flag.Parse()
+	ip := flag.Arg(0)
+	port := flag.Arg(1)
+	if ip == "" || port == "" {
+		fmt.Println("Usage: client ip port")
+		return
+	}
+
+	addr := ip + ":" + port
+
+	fmt.Printf("Connecting to %s ...", addr)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		fmt.Printf("Failed to connect due to: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Connected\n")
+
+	defer conn.Close()
+
+	cin := bufio.NewReader(os.Stdin)
+	buf := make([]byte, 64)
+
+	for {
+		msg, err := cin.ReadString('\n')
+		if err != nil {
+			fmt.Println("Bye-bye")
+			return
+		}
+
+		msg = strings.TrimRight(msg, "\n")
+
+		if _, err = conn.Write([]byte(msg)); err != nil {
+			fmt.Printf("Failed to write to %s due to: %v\n", addr, err)
+			break
+		}
+
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Printf("Failed to write to %s due to: %v\n", addr, err)
+			break
+		}
+
+		fmt.Println(string(buf[:n]))
+	}
 }
