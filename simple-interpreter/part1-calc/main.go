54189ef156c5be3a97b3295c132bf5af3bf7e562
Kingsley Chen <kingsamchen@gmail.com>
2019-10-06 16:47:50 +0800

    simple-interpreter: more complex calc
    1. supports multiple digits and skip spaces
    2. operand now can be + or -

--- simple-interpreter/part1-calc/main.go
+++ simple-interpreter/part1-calc/main.go
@@ -10,15 +10,18 @@ import (
 )
 
 /*
- * part1: assume operator and operand are only one character
- *        and no whitespaces can occur.
+ * part1: two operands and one operator
+ *        an operator can be {+, -}
+ *        operand can be multiple digits
+ *        automatically skip redundant spaces
  */
 
 const (
 	tokInteger = iota
 	tokPlus
+	tokMinus
 	tokEOF = -1
-	tokErr
+	tokUnknown
 )
 
 type token struct {
@@ -31,33 +34,79 @@ type expr struct {
 	pos  int
 }
 
-func (e *expr) nextToken() (tok token) {
+func isSpace(ch byte) bool {
+	return ch == ' ' || ch == '\t'
+}
+
+// returns current character or 0 if already past the end.
+func (e *expr) peek() byte {
 	if e.pos >= len(e.text) {
-		return token{
-			kind: tokEOF,
-		}
+		return 0
 	}
+	return e.text[e.pos]
+}
 
-	ch := e.text[e.pos]
-
-	if unicode.IsDigit(rune(ch)) {
+func (e *expr) skipSpace() {
+	for isSpace(e.peek()) {
 		e.pos++
-		return token{
-			kind:  tokInteger,
-			value: string(ch),
-		}
 	}
+}
 
-	if ch == '+' {
+func (e *expr) tokenizeInteger() string {
+	integer := ""
+	for unicode.IsDigit(rune(e.peek())) {
+		integer += string(e.peek())
 		e.pos++
+	}
+	return integer
+}
+
+// case 1: skip whitespaces
+// case 2: multiple digit
+// case 3: supports + and -
+func (e *expr) nextToken() (tok token) {
+	// Try to lexize each token in one loop.
+	// For spaces, it will locate to the first non-space and restart lexize again.
+	for e.pos < len(e.text) {
+		ch := e.peek()
+
+		if isSpace(ch) {
+			e.skipSpace()
+			continue
+		}
+
+		if unicode.IsDigit(rune(ch)) {
+			value := e.tokenizeInteger()
+			return token{
+				kind:  tokInteger,
+				value: value,
+			}
+		}
+
+		if ch == '+' {
+			e.pos++
+			return token{
+				kind:  tokPlus,
+				value: string(ch),
+			}
+		}
+
+		if ch == '-' {
+			e.pos++
+			return token{
+				kind:  tokMinus,
+				value: string(ch),
+			}
+		}
+
 		return token{
-			kind:  tokPlus,
+			kind:  tokUnknown,
 			value: string(ch),
 		}
 	}
 
 	return token{
-		kind: tokErr,
+		kind: tokEOF,
 	}
 }
 
@@ -76,9 +125,9 @@ func (e *expr) eval() (int, error) {
 		return 0, err
 	}
 
-	_, err = expectToken(e.nextToken(), tokPlus)
-	if err != nil {
-		return 0, err
+	op := e.nextToken()
+	if op.kind != tokPlus && op.kind != tokMinus {
+		return 0, fmt.Errorf("unexpected op token; op=%+v", &op)
 	}
 
 	right, err := expectToken(e.nextToken(), tokInteger)
@@ -88,7 +137,13 @@ func (e *expr) eval() (int, error) {
 
 	l, _ := strconv.Atoi(left.value)
 	r, _ := strconv.Atoi(right.value)
-	result := l + r
+
+	var result int
+	if op.kind == tokPlus {
+		result = l + r
+	} else {
+		result = l - r
+	}
 
 	return result, nil
 }
