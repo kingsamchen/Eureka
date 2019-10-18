d207aec6e75c3e88b03b843f3f3ff228954a1713
Kingsley Chen <kingsamchen@gmail.com>
2019-10-18 19:02:24 +0800

    simple-interpreter: add support of fundamental pascal program definition

---
+++ simple-interpreter/part6-spi-pascal-basic/main_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/smartystreets/goconvey/convey"
+)
+
+func TestMain(m *testing.M) {
+	m.Run()
+}
+
+func TestLexer_PascalDefinitions(t *testing.T) {
+	convey.Convey("Get tokens for pascal definitions", t, func(c convey.C) {
+		lex := newLexer("BEGIN foo := 2; END.")
+		var tok token
+
+		tok = lex.currentToken()
+		c.So(tok.kind, convey.ShouldEqual, tokBegin)
+		c.So(tok.value, convey.ShouldEqual, "BEGIN")
+
+		lex.nextToken()
+		tok = lex.currentToken()
+		c.So(tok.kind, convey.ShouldEqual, tokVar)
+		c.So(tok.value, convey.ShouldEqual, "foo")
+
+		lex.nextToken()
+		tok = lex.currentToken()
+		c.So(tok.kind, convey.ShouldEqual, tokAssign)
+		c.So(tok.value, convey.ShouldEqual, ":=")
+
+		lex.nextToken()
+		tok = lex.currentToken()
+		c.So(tok.kind, convey.ShouldEqual, tokInteger)
+		c.So(tok.value, convey.ShouldEqual, "2")
+
+		lex.nextToken()
+		tok = lex.currentToken()
+		c.So(tok.kind, convey.ShouldEqual, tokSemi)
+		c.So(tok.value, convey.ShouldEqual, ";")
+
+		lex.nextToken()
+		tok = lex.currentToken()
+		c.So(tok.kind, convey.ShouldEqual, tokEnd)
+		c.So(tok.value, convey.ShouldEqual, "END")
+
+		lex.nextToken()
+		tok = lex.currentToken()
+		c.So(tok.kind, convey.ShouldEqual, tokDot)
+		c.So(tok.value, convey.ShouldEqual, ".")
+
+		lex.nextToken()
+		c.So(lex.currentToken().kind, convey.ShouldEqual, tokEOF)
+	})
+}
+
+func TestParser_PascalDefinitions(t *testing.T) {
+	convey.Convey("", t, func(c convey.C) {
+		parser := newParser("BEGIN foo := 2; END.")
+		node, err := parser.parse()
+		c.So(err, convey.ShouldBeNil)
+		_, ok := node.(*nodeCompound)
+		c.So(ok, convey.ShouldBeTrue)
+	})
+}
