b35a326e286ece25b46891acdc853e94d46012a3
Kingsley Chen <kingsamchen@gmail.com>
2020-05-05 23:24:37 +0800

    Make demo directory structure and pass build

---
+++ study-crow-framework/app/main.cpp
@@ -0,0 +1,14 @@
+
+#include "crow.h"
+
+int main()
+{
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")([](){
+        return "Hello world";
+    });
+
+    app.port(18080).multithreaded().run();
+    return 0;
+}
