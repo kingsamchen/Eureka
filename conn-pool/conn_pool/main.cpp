bb1f7413353552a098d07aa4f519d90417562671
Kingsley Chen <kingsamchen@gmail.com>
2020-09-03 22:32:07 +0800

    conn-pool: Get & Put

--- conn-pool/conn_pool/main.cpp
+++ conn-pool/conn_pool/main.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 
+#include "kbase/basic_macros.h"
+#include "kbase/logging.h"
+
 #include "conn_pool/pool.h"
 
 struct RedisConn {
     RedisConn()
     {
         std::cout << "RedisConn ctor\n";
+    }
+
+    DISALLOW_COPY(RedisConn);
+    DEFAULT_MOVE(RedisConn);
+
+    void Set(const std::string& key, const std::string& value)
+    {
+        FORCE_AS_MEMBER_FUNCTION();
+        LOG(INFO) << "SET " << key << " " << value;
+    }
+
+    std::string Get(const std::string& key)
+    {
+        FORCE_AS_MEMBER_FUNCTION();
+        LOG(INFO) << "GET " << key;
+        return {};
     }
 };
 
@@ -20,13 +39,20 @@ struct RedisConnTraits {
 
 int main()
 {
+    kbase::LoggingSettings settings;
+    settings.logging_destination = kbase::LogToSystemDebugLog;
+    kbase::ConfigureLoggingSettings(settings);
+
     auto redis_pool = ConnPool<RedisConnTraits>::Make(100, 10, std::chrono::seconds(10));
 
     {
         auto conn = redis_pool->Get();
-        std::cout << "use with conn\n";
+
+        conn->Set("abc", "daf");
+        IGNORE_RESULT(conn->Get("abc"));
+
+        LOG(INFO) << "Done using conn";
     }
 
-    std::cout << "hello world" << std::endl;
     return 0;
 }
--- conn-pool/conn_pool/pool.h
+++ conn-pool/conn_pool/pool.h
@@ -10,39 +10,85 @@
 #include <memory>
 #include <mutex>
 #include <type_traits>
+#include <vector>
 
 #include "kbase/basic_macros.h"
+#include "kbase/logging.h"
 
 namespace chrono = std::chrono;
 
+class PoolExhausted : public std::runtime_error {
+public:
+    explicit PoolExhausted(const std::string& what)
+        : std::runtime_error(what)
+    {}
+
+    explicit PoolExhausted(const char* what)
+        : std::runtime_error(what)
+    {}
+};
+
 template<typename Traits>
 class ConnPool : public std::enable_shared_from_this<ConnPool<Traits>> {
+public:
+    using RawConnType = typename Traits::ConnType;
+
+    static_assert(std::is_move_constructible_v<RawConnType> &&
+                  std::is_move_assignable_v<RawConnType>,
+                  "Traits::ConnType must be move-constructible");
+
 private:
+    using clock = chrono::system_clock;
+
+    // For passkey idiom.
     class Passkey {
         explicit Passkey() = default;
         friend ConnPool;
     };
 
+    struct IdleConn {
+        RawConnType conn;
+        clock::time_point created;
+        clock::time_point last_active;
+
+        IdleConn(RawConnType conn, clock::time_point created)
+            : conn(std::move(conn)),
+              created(created),
+              last_active(clock::now())
+        {}
+
+        ~IdleConn() = default;
+
+        DISALLOW_COPY(IdleConn);
+
+        DEFAULT_MOVE(IdleConn);
+    };
+
 public:
-    using RawConnType = typename Traits::ConnType;
-
-    static_assert(std::is_move_constructible_v<RawConnType>,
-                  "Traits::ConnType must be move-constructible");
-
     class Conn {
     public:
         using Pool = ConnPool<Traits>;
+        using pointer = RawConnType*;
+        using reference = RawConnType&;
 
         Conn(RawConnType raw_conn, const std::shared_ptr<Pool>& pool)
             : raw_conn_(std::move(raw_conn)),
+              raw_conn_creation_(clock::now()),
+              bound_pool_(pool)
+        {}
+
+        Conn(IdleConn&& idle, const std::shared_ptr<Pool>& pool)
+            : raw_conn_(std::move(idle.conn)),
+              raw_conn_creation_(idle.created),
               bound_pool_(pool)
         {}
 
         ~Conn()
         {
+            // TODO: before putting back, must resolve its state like in pipelining
             auto pool = bound_pool_.lock();
             if (pool) {
-                pool->Put(std::move(raw_conn_));
+                pool->Put(std::move(raw_conn_), raw_conn_creation_);
             }
         }
 
@@ -50,8 +96,19 @@ public:
 
         DEFAULT_MOVE(Conn);
 
+        pointer operator->() noexcept
+        {
+            return &raw_conn_;
+        }
+
+        reference operator*() noexcept
+        {
+            return raw_conn_;
+        }
+
     private:
         RawConnType raw_conn_;
+        clock::time_point raw_conn_creation_;
         std::weak_ptr<Pool> bound_pool_;
     };
 
@@ -84,15 +141,70 @@ public:
             chrono::duration_cast<chrono::microseconds>(idle_timeout));
     }
 
-    // TODO: Must be thread-safe
+    // Takes a connection from the pool; create a new underlying connection in the fly,
+    // if necessary.
+    // This function throws on failure.
+    // This function is thread-safe.
     Conn Get()
     {
-        return Conn(Traits::NewConn(), this->shared_from_this());
+        if (idle_timeout_.count() > 0) {
+            RetireTimeoutIdleConnections();
+        }
+
+        {
+            std::lock_guard lock(mtx_);
+
+            // Try to reuse from idle connections first.
+            // The pool's size keeps unchanged.
+            if (!idle_list_.empty()) {
+                Conn conn(std::move(idle_list_.back()), this->shared_from_this());
+                idle_list_.pop_back();
+                return conn;
+            }
+
+            if (size_ >= capacity_) {
+                throw PoolExhausted("the pool is exhausted");
+            }
+
+            // We are about to create a new underlying connection.
+            ++size_;
+        }
+
+        try {
+            return Conn(Traits::NewConn(), this->shared_from_this());
+        } catch (const std::exception& ex) {
+            {
+                std::lock_guard lock(mtx_);
+                --size_;
+            }
+
+            LOG(ERROR) << "Failed to call Traits::NewConn; ex=" << ex.what();
+            throw;
+        }
     }
 
-    // TODO: Must be thread-safe
-    void Put(RawConnType)
-    {}
+    // Puts a connection back into the pool.
+    // Called by Conn during its destruction.
+    // This function is thread-safe.
+    void Put(RawConnType raw_conn, clock::time_point creation)
+    {
+        // Unluckily, we cannot assume IdleConn can have a default constructor.
+        std::unique_ptr<IdleConn> to_retire;
+
+        {
+            std::lock_guard lock(mtx_);
+
+            if (idle_list_.size() == max_idle_) {
+                to_retire = std::make_unique<IdleConn>(std::move(idle_list_.front()));
+                idle_list_.pop_front();
+                --size_;
+            }
+
+            idle_list_.push_back(IdleConn(std::move(raw_conn), creation));
+        }
+
+        to_retire.reset();
+    }
 
     size_t capacity() const noexcept
     {
@@ -117,13 +229,30 @@ public:
     }
 
 private:
+    void RetireTimeoutIdleConnections()
+    {
+        std::vector<IdleConn> to_retire;
+        auto now = clock::now();
+
+        {
+            std::lock_guard lock(mtx_);
+            while (!idle_list_.empty() && (now - idle_list_.front().last_active >= idle_timeout_)) {
+                to_retire.push_back(std::move(idle_list_.front()));
+                idle_list_.pop_front();
+                --size_;
+            }
+        }
+    }
+
+private:
     const size_t capacity_;
     const size_t max_idle_;
     const chrono::microseconds idle_timeout_;
 
     mutable std::mutex mtx_;
     size_t size_;
-    std::deque<RawConnType> idle_list_;
+    // Most inactive conn is at the head.
+    std::deque<IdleConn> idle_list_;
 };
 
 #endif  // CONN_POOL_POOL_H_
