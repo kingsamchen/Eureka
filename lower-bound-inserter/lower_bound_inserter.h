32681e1287d6f5f656970768fd3ccb761fe36743
Kingsley Chen <kingsamchen@gmail.com>
2021-01-07 01:28:44 +0800

    Add lower-bound-inserter

---
+++ lower-bound-inserter/lower_bound_inserter.h
@@ -0,0 +1,73 @@
+﻿#ifndef LOWER_BOUND_INSERTER_H_
+#define LOWER_BOUND_INSERTER_H_
+
+#include <algorithm>
+#include <functional>
+#include <iterator>
+
+namespace ext {
+
+template<typename Container>
+class lower_bound_insert_iterator {
+public:
+    using iterator_category = std::output_iterator_tag;
+    using value_type = void;
+    using difference_type = void;
+    using pointer = void;
+    using reference = void;
+
+    using Compare = std::function<bool(const typename Container::value_type& lhs,
+                                       const typename Container::value_type& rhs)>;
+
+    lower_bound_insert_iterator() noexcept
+        : container_(nullptr)
+    {}
+
+    explicit lower_bound_insert_iterator(Container& c, Compare cmp) noexcept
+        : container_(std::addressof(c)),
+          cmp_(std::move(cmp))
+    {}
+
+    lower_bound_insert_iterator& operator=(const typename Container::value_type& value)
+    {
+        auto it = std::lower_bound(std::begin(*container_), std::end(*container_), value, cmp_);
+        container_->insert(it, value);
+        return *this;
+    }
+
+    lower_bound_insert_iterator& operator=(typename Container::value_type&& value)
+    {
+        auto it = std::lower_bound(std::begin(*container_), std::end(*container_), value, cmp_);
+        container_->insert(it, std::move(value));
+        return *this;
+    }
+
+    lower_bound_insert_iterator& operator*() noexcept
+    {
+        return *this;
+    }
+
+    lower_bound_insert_iterator& operator++() noexcept
+    {
+        return *this;
+    }
+
+    lower_bound_insert_iterator operator++(int)
+    {
+        return *this;
+    }
+
+private:
+    Container* container_;
+    Compare cmp_;
+};
+
+template<typename Container, typename Cmp = std::less<>>
+[[nodiscard]] auto lower_bound_inserter(Container& c, Cmp&& cmp = Cmp{})
+{
+    return lower_bound_insert_iterator<Container>(c, std::forward<Cmp>(cmp));
+}
+
+}   // namespace ext
+
+#endif  // LOWER_BOUND_INSERTER_H_
---
+++ lower-bound-inserter/main.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <vector>
+
+#include "lower_bound_inserter.h"
+
+void StandardCase()
+{
+    std::vector<int> major = {1, 3, 5, 7, 9};
+    std::vector<int> sub = {3, 2, 4, 6};
+    std::copy(sub.begin(), sub.end(), ext::lower_bound_inserter(major));
+    std::copy(major.begin(), major.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << "\n";
+}
+
+void DecreaseOrderCase()
+{
+    std::vector<int> major{9, 7, 5, 3, 1};
+    std::vector<int> sub{2, 4, 3, 6};
+    auto greater = [](const auto& lhs, const auto& rhs) { return rhs < lhs; };
+    std::copy(sub.begin(), sub.end(), ext::lower_bound_inserter(major, greater));
+    std::copy(major.begin(), major.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << "\n";
+}
+
+int main()
+{
+    StandardCase();
+    DecreaseOrderCase();
+    return 0;
+}
