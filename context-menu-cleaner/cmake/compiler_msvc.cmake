ee4c1db043d618d10bdb1bfe70fae480f763a7fd
Kingsley Chen <kingsamchen@gmail.com>
2021-01-16 16:05:39 +0800

    context-menu-cleaner: Introduce nana-gui as our gui library

--- context-menu-cleaner/cmake/compiler_msvc.cmake
+++ context-menu-cleaner/cmake/compiler_msvc.cmake
@@ -1,6 +1,8 @@
 
 option(MSVC_ENABLE_PARALLEL_BUILD "If enabled, build multiple files in parallel." ON)
 
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /permissive-")
+
 # Force generating debugging symbols in Release build.
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /Zi")
 set(CMAKE_EXE_LINKER_FLAGS_RELEASE "${CMAKE_EXE_LINKER_FLAGS_RELEASE} /DEBUG /OPT:REF /OPT:ICF")
