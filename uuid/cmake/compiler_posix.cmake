5bf31c12fe516f4bff30dc7fdfc8cbd0e59aff36
Kingsley Chen <kingsamchen@gmail.com>
2021-02-22 23:34:06 +0800

    uuid: Init the project

---
+++ uuid/cmake/compiler_posix.cmake
@@ -0,0 +1,32 @@
+
+# Force generating debugging symbols in Release build.
+# Also keep STL debugging symbols for clang builds.
+set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -g")
+if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-limit-debug-info")
+endif()
+
+function(apply_uuid_compile_conf TARGET)
+  target_compile_definitions(${TARGET}
+    PUBLIC
+      $<$<CONFIG:DEBUG>:
+        _DEBUG
+      >
+  )
+
+  target_compile_options(${TARGET}
+    PRIVATE
+      -Wall
+      -Wextra
+      -Wno-unused-parameter
+      -Wold-style-cast
+      -Woverloaded-virtual
+      -Wpointer-arith
+      -Wshadow
+  )
+
+  set_target_properties(${TARGET} PROPERTIES
+    LINK_FLAGS "-rdynamic" # Currently required by backtrace_symbols(2)
+  )
+
+endfunction()
