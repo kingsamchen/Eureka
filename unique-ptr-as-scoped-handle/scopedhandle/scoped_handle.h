c3f83e42a5e0f506cd5ffe079b7e6bf54258363a
Kingsley Chen <kingsamchen@gmail.com>
2021-06-14 16:46:04 +0800

    unique-ptr-as-scoped-handle: complete poc

--- unique-ptr-as-scoped-handle/scopedhandle/scoped_handle.h
+++ unique-ptr-as-scoped-handle/scopedhandle/scoped_handle.h
@@ -4,8 +4,19 @@
 
 #include <cstdio>
 #include <memory>
+#include <type_traits>
+
+#if defined(_WIN32) || defined(_WIN64)
+#include <Windows.h>
+#endif
 
 namespace scoped {
+namespace details {
+
+template<typename T>
+void ignore_result(T&&) {}
+
+} // namespace details
 
 struct c_file_deleter {
     void operator()(FILE* ptr) const {
@@ -13,7 +24,55 @@ struct c_file_deleter {
     }
 };
 
+static_assert(std::is_empty_v<c_file_deleter>);
+
 using scoped_file = std::unique_ptr<FILE, c_file_deleter>;
+
+// -*- For Win32 HANDLE -*-
+
+class win_handle {
+public:
+    win_handle() noexcept = default;
+
+    // implicit
+    win_handle(std::nullptr_t) noexcept {}
+
+    // implicit
+    win_handle(HANDLE h)
+        : handle_(h) {}
+
+    explicit operator bool() const noexcept {
+        return handle_ != nullptr && handle_ != INVALID_HANDLE_VALUE;
+    }
+
+    // implicit
+    operator HANDLE() const noexcept {
+        return handle_;
+    }
+
+    friend bool operator==(win_handle lhs, win_handle rhs) noexcept {
+        return lhs.handle_ == rhs.handle_;
+    }
+
+    friend bool operator!=(win_handle lhs, win_handle rhs) noexcept {
+        return !(lhs == rhs);
+    }
+
+private:
+    HANDLE handle_ = nullptr;
+};
+
+struct win_handle_deleter {
+    using pointer = win_handle;
+
+    void operator()(pointer ptr) const {
+        details::ignore_result(CloseHandle(ptr));
+    }
+};
+
+static_assert(std::is_empty_v<win_handle_deleter>);
+
+using scoped_win_handle = std::unique_ptr<win_handle, win_handle_deleter>;
 
 } // namespace scoped
 
--- unique-ptr-as-scoped-handle/test/scoped_handle_test.cpp
+++ unique-ptr-as-scoped-handle/test/scoped_handle_test.cpp
@@ -9,6 +9,7 @@ namespace scoped {
 
 TEST_CASE("For C FILE", "[FILE]") {
     scoped_file file(std::fopen("test.txt", "w"));
+    REQUIRE(sizeof(file) == sizeof(void*));
     REQUIRE(!!file);
 
     std::string msg = "this is a test text";
@@ -19,6 +20,30 @@ TEST_CASE("For C FILE", "[FILE]") {
 
     file = nullptr;
     REQUIRE(!file);
+}
+
+TEST_CASE("For Windows Handle", "[windows-handle]") {
+    scoped_win_handle handle;
+    REQUIRE(!handle);
+
+    SECTION("equivalent to one that being initialized with INVALID_HANDLE_VALUE") {
+        scoped_win_handle invalid_handle(INVALID_HANDLE_VALUE);
+        CHECK(handle != invalid_handle);
+        CHECK((!handle && !invalid_handle));
+    }
+
+    SECTION("unique_ptr abilities") {
+        handle.reset(CreateEventW(nullptr, TRUE, FALSE, L"test_ev"));
+        REQUIRE(!!handle);
+
+        auto dup = std::move(handle);
+        CHECK(!handle);
+        CHECK(!!dup);
+    } // the Event kernel object is destroyed at here.
+
+    handle.reset(OpenEventW(EVENT_ALL_ACCESS, FALSE, L"test_ev"));
+    CHECK(!handle);
+    CHECK(GetLastError() == ERROR_FILE_NOT_FOUND);
 }
 
 } // namespace scoped
