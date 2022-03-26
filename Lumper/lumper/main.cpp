9714fb799d29eec5189b0b05cfa9d907794d26d6
Kingsley Chen <kingsamchen@gmail.com>
2022-03-26 23:56:27 +0800

    Lumper: short reference to the cli

--- Lumper/lumper/main.cpp
+++ Lumper/lumper/main.cpp
@@ -6,8 +6,10 @@
 
 #include "fmt/ranges.h"
 
-void process(lumper::cli::cmd_run_t) {
-    const auto& parser = lumper::cli::for_current_process().command_parser();
+using lumper::cli;
+
+void process(cli::cmd_run_t) {
+    const auto& parser = cli::for_current_process().command_parser();
     auto maybe_args = parser.present<std::vector<std::string>>("ARGS");
     fmt::print("cmd={}\nargs={}\n--it={}",
                parser.get<std::string>("CMD"),
@@ -15,13 +17,13 @@ void process(lumper::cli::cmd_run_t) {
                parser.get<bool>("--it"));
 }
 
-void process(lumper::cli::cmd_ps_t) {
+void process(cli::cmd_ps_t) {
     fmt::print("lumper ps!");
 }
 
 int main(int argc, const char* argv[]) {
-    lumper::cli::init(argc, argv);
-    lumper::cli::for_current_process().process_command([](auto cmd) {
+    cli::init(argc, argv);
+    cli::for_current_process().process_command([](auto cmd) {
         process(cmd);
     });
     return 0;
