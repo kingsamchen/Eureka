a738ff717fbed147afc89a43653b759f6edac604
Kingsley Chen <kingsamchen@gmail.com>
2022-05-05 00:39:04 +0800

    Sping-boot test

--- try-spring-boot/src/main/java/com/example/tryspringboot/GreetingController.java
+++ try-spring-boot/src/main/java/com/example/tryspringboot/GreetingController.java
@@ -2,6 +2,7 @@ package com.example.tryspringboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -11,8 +12,9 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
     private static final String TEMPLATE = "Hello, %s!";
 
-    @GetMapping("/greeting")
-    public Greeter greeting(@RequestParam(value="name", defaultValue = "") String name) {
+    @GetMapping("/hello-world")
+    @ResponseBody
+    public Greeter sayHello(@RequestParam(value = "name", defaultValue = "Stranger", required = false) String name) {
         return new Greeter(counter.incrementAndGet(), String.format(TEMPLATE, name));
     }
 }
