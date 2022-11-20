d11a81ca52c77e395f48ffc08dc697619cbf16df
Kingsley Chen <kingsamchen@gmail.com>
2022-11-20 22:46:56 +0800

    bloom-filter: add() and may_contain()

--- bloom-filter/bloom-filter/bloom_filter.cpp
+++ bloom-filter/bloom-filter/bloom_filter.cpp
@@ -10,8 +10,22 @@
 
 #include "fmt/format.h"
 
+#include "bloom-filter/MurmurHash2.h"
+
 namespace bf {
 namespace {
+
+struct hash_result {
+    std::uint32_t a;
+    std::uint32_t b;
+};
+
+hash_result calc_data_hash(const void* data, int len) noexcept {
+    hash_result result; // NOLINT(cppcoreguidelines-pro-type-member-init)
+    result.a = MurmurHash2(data, len, 0x9747b28c);
+    result.b = MurmurHash2(data, len, result.a);
+    return result;
+}
 
 // Optimal bpe = -(ln(error) / ln(2)^2).
 // Throws: `std::invalid_argument`.
@@ -80,6 +94,65 @@ bloom_filter::bloom_filter(std::uint32_t estimated_max_entries, double error)
     num_bits_ = calc_capacity(estimated_max_entries, bits_per_entry_);
     max_entries_ = static_cast<std::size_t>(static_cast<double>(num_bits_) / bits_per_entry_);
     bytes_.resize(num_bits_ / 8);
+}
+
+bool bloom_filter::add(const void* data, std::int32_t len) {
+    if (len <= 0) {
+        throw std::invalid_argument("len must be positive");
+    }
+
+    auto hashval = calc_data_hash(data, len);
+    bool added = check_add_hash(hashval.a, hashval.b, access_mode::write);
+    if (added) {
+        ++size_;
+    }
+
+    return added;
+}
+
+bool bloom_filter::may_contain(const void* data, std::int32_t len) {
+    if (len <= 0) {
+        throw std::invalid_argument("len must be positive");
+    }
+
+    auto hashval = calc_data_hash(data, len);
+    return check_add_hash(hashval.a, hashval.b, access_mode::read);
+}
+
+bool bloom_filter::check_add_hash(std::uint64_t hv_a, std::uint64_t hv_b, access_mode mode) {
+    bool unset_found = false;
+    // In read mode, one iteration of unset found is enough to indicate the absent of the data,
+    // while in write mode, one iteration of unset found is also enough to indicate the data is
+    // new, but still needs to set bit in all iteration.
+    for (std::uint64_t i = 0; i < hash_count(); ++i) {
+        std::uint64_t idx = (hv_a + i * hv_b) % num_bits_;
+        if (!test_set_bit(idx, mode)) {
+            if (mode == access_mode::read) {
+                return false;
+            }
+            unset_found = true;
+        }
+    }
+
+    if (mode == access_mode::read) {
+        return true;
+    }
+
+    return unset_found;
+}
+
+bool bloom_filter::test_set_bit(std::uint64_t bit_index, access_mode mode) {
+    std::uint64_t byte_index = bit_index / 8;
+    auto mask = std::byte{1} << (bit_index % 8);
+    if ((bytes_[byte_index] & mask) != std::byte{0}) {
+        return true;
+    }
+
+    if (mode == access_mode::write) {
+        bytes_[byte_index] |= mask;
+    }
+
+    return false;
 }
 
 } // namespace bf
--- bloom-filter/bloom-filter/bloom_filter.h
+++ bloom-filter/bloom-filter/bloom_filter.h
@@ -14,12 +14,17 @@
 namespace bf {
 
 class bloom_filter {
+    enum class access_mode {
+        read,
+        write
+    };
+
 public:
     // Throws:
-    // - `std::invalid_argument` if `estimated_max_entries` is not positive or `error` is not
-    //   within (0, 1).
-    // - `std::length_error` if calculated capacity is more than 2^50 bytes, which should not
-    //   happen in almost any case.
+    //  - `std::invalid_argument` if `estimated_max_entries` is not positive or `error` is not
+    //    within (0, 1).
+    //  - `std::length_error` if calculated capacity is more than 2^50 bytes, which should not
+    //    happen in almost any case.
     bloom_filter(std::uint32_t estimated_max_entries, double error);
 
     bloom_filter(const bloom_filter&) = default;
@@ -32,17 +37,45 @@ public:
 
     ~bloom_filter() = default;
 
+    // Returns true if data is added.
+    // Returns false if data may already be present.
+    // Throws: `std::invalid_argument` if `len` is not positive.
+    bool add(const void* data, std::int32_t len);
+
+    // Returns true if data **maybe** contained.
+    // Returns false if data is **absolutely not** contained.
+    // Throws: `std::invalid_argument` if `len` is not positive.
+    bool may_contain(const void* data, std::int32_t len);
+
     std::size_t max_entries() const noexcept {
         return max_entries_;
     }
 
+    std::uint32_t hash_count() const noexcept {
+        return hashes_;
+    }
+
+    std::size_t size() const noexcept {
+        return size_;
+    }
+
+private:
+    // In read mode, returns true if hashval is present, returns false otherwise.
+    // In write mode, returns true if hashval is added, returns false otherwise.
+    // Use 64-bit integer for hash values to prevent data overflow during calculation.
+    bool check_add_hash(std::uint64_t hv_a, std::uint64_t hv_b, access_mode mode);
+
+    // Returns true, if the bit at `bit_index` has already been set.
+    // Returns false, otherwise.
+    bool test_set_bit(std::uint64_t bit_index, access_mode mode);
+
 private:
     double error_;
     double bits_per_entry_;
-    std::uint32_t hashes_;
-    std::uint64_t num_bits_;
-    std::size_t max_entries_;
-    std::size_t size_;
+    std::uint32_t hashes_;    // optimal hash number
+    std::uint64_t num_bits_;  // always in power of 2
+    std::size_t max_entries_; // optimal max entries with respect to speficied `error_`
+    std::size_t size_;        // current count of entries
     std::vector<std::byte> bytes_;
 };
 
