84cfd36058cd5b4bf23fc907011fccc8a448a361
Kingsley Chen <kingsamchen@gmail.com>
2024-06-25 13:02:14 +0800

    learn-cxx-coroutine: lazy coro future

--- learn-cxx-coroutine/coro_task/future.h
+++ learn-cxx-coroutine/coro_task/future.h
@@ -5,36 +5,28 @@
 #pragma once
 
 #include <coroutine>
-#include <memory>
+#include <cstdlib>
 
 namespace coro {
 
 template<typename T>
 class future {
-public:
-    explicit future(std::shared_ptr<T> v)
-        : value_(std::move(v)) {}
-
-    const T& get() {
-        return *value_;
-    }
-
-    struct promise_type {
-        std::shared_ptr<T> ptr = std::make_shared<T>();
+    struct promise {
+        T value;
 
         future<T> get_return_object() {
-            return future(ptr);
+            return future(std::coroutine_handle<promise>::from_promise(*this));
         }
 
         void return_value(const T& v) {
-            *ptr = v;
+            value = v;
         }
 
-        std::suspend_never initial_suspend() noexcept {
+        std::suspend_always initial_suspend() noexcept {
             return {};
         }
 
-        std::suspend_never final_suspend() noexcept {
+        std::suspend_always final_suspend() noexcept {
             return {};
         }
 
@@ -43,8 +35,32 @@ public:
         }
     };
 
+public:
+    using promise_type = promise;
+    using handle_type = std::coroutine_handle<promise_type>;
+
+    explicit future(handle_type handle) noexcept
+        : handle_(handle) {}
+
+    ~future() {
+        if (handle_) {
+            handle_.destroy();
+        }
+    }
+
+    // Disallow copy and move(so far)
+    future(const future&) = delete;
+    future(future&&) = delete;
+    future& operator=(const future&) = delete;
+    future& operator=(future&&) = delete;
+
+    T get() {
+        handle_.resume();
+        return handle_.promise().value;
+    }
+
 private:
-    std::shared_ptr<T> value_;
+    handle_type handle_;
 };
 
 } // namespace coro
