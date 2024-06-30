92abb70ee03a46a4c9a8f06bc9decda90f8cc214
Kingsley Chen <kingsamchen@gmail.com>
2024-06-30 23:07:11 +0800

    learn-cxx-coroutine: resume coroutine on another thread

--- learn-cxx-coroutine/coro_task/main.cpp
+++ learn-cxx-coroutine/coro_task/main.cpp
@@ -3,27 +3,28 @@
 //
 
 #include <thread>
+#include <vector>
 
 #include "fmt/core.h"
 #include "fmt/std.h"
 
 // #include "coro_task/future.h"
-#include "coro_task/job.h"
+// #include "coro_task/job.h"
+#include "coro_task/job2.h"
 
 // coro::future<int> create_future() {
 //     co_return 42;
 // }
 
-coro::job prepare_job() {
-    fmt::println("{}: {}", __PRETTY_FUNCTION__, std::this_thread::get_id());
-    co_await coro::my_suspend_never{};
-}
+// coro::job prepare_job() {
+//     fmt::println("{}: {}", __PRETTY_FUNCTION__, std::this_thread::get_id());
+//     co_await coro::my_suspend_never{};
+// }
 
 int main() {
-    fmt::println("{}: {}", __PRETTY_FUNCTION__, std::this_thread::get_id());
-    auto job = prepare_job();
-    fmt::println("start the job");
-    job.start();
-    fmt::println("after the job");
+    std::vector<std::jthread> thds(8);
+    for (auto& thd : thds) {
+        coro::perform_job2(thd);
+    }
     return 0;
 }
