8e111f358534a1ce663950ceeaef8c78f7e5e6d5
Kingsley Chen <kingsamchen@gmail.com>
2024-07-08 17:25:15 +0800

    Add cpr-struct-parameters poc

---
+++ cpr-struct-parameters/cmake/compiler_posix.cmake
@@ -0,0 +1,36 @@
+
+if(CPR_STRUCT_PARAMETERS_NOT_SUBPROJECT)
+  message(STATUS "cpr_struct_parameters compiler POSIX global conf is in active")
+
+  # Force generating debugging symbols in Release build.
+  # Also keep STL debugging symbols for clang builds.
+  set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -g")
+  if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-limit-debug-info")
+  endif()
+endif()
+
+function(cpr_struct_parameters_apply_common_compile_options TARGET)
+  target_compile_definitions(${TARGET}
+    PRIVATE
+      $<$<CONFIG:DEBUG>:
+        _DEBUG
+      >
+  )
+
+  target_compile_options(${TARGET}
+    PRIVATE
+      -Wall
+      -Wextra
+      -Werror
+      -Wconversion
+      -Wdouble-promotion
+      -Wold-style-cast
+      -Woverloaded-virtual
+      -Wpointer-arith
+      -Wshadow
+      -Wsign-conversion
+      -Wno-unused-function
+      -Wno-error=deprecated
+  )
+endfunction()
