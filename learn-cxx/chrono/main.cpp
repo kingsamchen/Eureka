dd8175c1d3a11f62c2a082717639c8322299468f
Kingsley Chen <kingsamchen@gmail.com>
2025-05-27 13:39:29 +0800

    Use C++ 20 datetime

--- learn-cxx/chrono/main.cpp
+++ learn-cxx/chrono/main.cpp
@@ -1,8 +1,10 @@
 
 #include <chrono>
 
-#include "fmt/chrono.h"
-#include "fmt/format.h"
+#include <date/date.h>
+#include <date/tz.h>
+#include <fmt/chrono.h>
+#include <fmt/format.h>
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest/doctest.h"
@@ -17,7 +19,7 @@ TEST_CASE("custom time point with 20mins resolution") {
 #if __cpp_lib_chrono >= 201907L
 
 TEST_CASE("display datetime in local timezone") {
-    auto my_timezone = std::chrono::current_zone();
+    const auto* my_timezone = std::chrono::current_zone();
     auto local_tp = my_timezone->to_local(std::chrono::system_clock::now());
 
     constexpr intmax_t res = 1200;
@@ -28,3 +30,77 @@ TEST_CASE("display datetime in local timezone") {
 }
 
 #endif
+
+// NOLINTBEGIN(readability-magic-numbers)
+
+TEST_CASE("Convert year_month_day object to sys_days") {
+    auto chloe_dob = date::year_month_day(date::year(2025), date::month(5), date::day(22));
+    // The construction of year_month_day object may be invalid.
+    REQUIRE(chloe_dob.ok());
+    auto chloe_dob_tp = date::sys_days(chloe_dob);
+    auto me_dob = date::year_month_day(date::year(1992), date::month(7), date::day(26));
+    auto me_dob_tp = date::sys_days(me_dob);
+    CHECK_EQ((chloe_dob_tp - me_dob_tp).count(), 11988);
+}
+
+TEST_CASE("Validness of year_month_day") {
+    CHECK(date::year_month_day(date::year(2024), date::month(2), date::day(29)).ok());
+    CHECK_FALSE(date::year_month_day(date::year(2025), date::month(2), date::day(30)).ok());
+    CHECK_FALSE(date::year_month_day(date::year(2025), date::month(13), date::day(1)).ok());
+}
+
+TEST_CASE("The last day of the month") {
+    SUBCASE("last day of feb in a leap year is 29") {
+        auto eom = date::year_month_day_last(date::year(2024),
+                                             date::month_day_last(date::month(2)));
+        REQUIRE(eom.ok());
+        CHECK_EQ(static_cast<unsigned>(eom.day()), 29);
+    }
+
+    SUBCASE("compose last day with more intuitive operator overload") {
+        auto eom = date::year(2025) / date::May / date::last;
+        REQUIRE(eom.ok());
+        CHECK_EQ(static_cast<unsigned>(eom.day()), 31);
+    }
+}
+
+TEST_CASE("Add to year_month_day") {
+    SUBCASE("add years") {
+        auto ymd = date::year(2024) / date::month(2) / date::day(1);
+        REQUIRE(ymd.ok());
+        ymd += date::years(1);
+        CHECK(ymd.ok());
+        CHECK_EQ(static_cast<int>(ymd.year()), 2025);
+    }
+
+    SUBCASE("add years but invalid") {
+        auto ymd = date::year(2024) / date::month(2) / date::day(29);
+        REQUIRE(ymd.ok());
+        ymd += date::years(1);
+        CHECK_FALSE(ymd.ok());
+    }
+
+    SUBCASE("add months") {
+        auto ymd = date::year(2024) / date::month(2) / date::day(1);
+        REQUIRE(ymd.ok());
+        ymd += date::months(1);
+        CHECK(ymd.ok());
+        CHECK_EQ(static_cast<unsigned int>(ymd.month()), 3);
+    }
+
+    SUBCASE("add months but invalid") {
+        auto ymd = date::year(2024) / date::month(1) / date::day(30);
+        REQUIRE(ymd.ok());
+        ymd += date::months(1);
+        CHECK_FALSE(ymd.ok());
+    }
+}
+
+TEST_CASE("Add days to year_month_day") {
+    auto ymd = date::year(2024) / date::month(2) / date::day(29);
+    REQUIRE(ymd.ok());
+    ymd = date::sys_days(ymd) + date::days(2);
+    CHECK_EQ(ymd, date::year(2024) / date::month(3) / date::day(2));
+}
+
+// NOLINTEND(readability-magic-numbers)
