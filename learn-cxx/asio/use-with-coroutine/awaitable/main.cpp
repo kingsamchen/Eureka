8c1b3ccaa0bad4c6e4059df28b56e44b98500f15
Kingsley Chen <kingsamchen@gmail.com>
2026-01-11 00:20:49 +0800

    Add lambda coroutine using awaitable

--- learn-cxx/asio/use-with-coroutine/awaitable/main.cpp
+++ learn-cxx/asio/use-with-coroutine/awaitable/main.cpp
@@ -3,7 +3,7 @@
 #include <thread>
 
 #include <asio.hpp>
-#include <fmt/std.h>
+#include <fmt/format.h>
 #include <spdlog/spdlog.h>
 
 namespace {
@@ -28,6 +28,37 @@ asio::awaitable<void> launcher() {
     co_return;
 }
 
+struct Tracker { // NOLINT(cppcoreguidelines-special-member-functions)
+    int val;
+    explicit Tracker(int v) : val(v) {}
+    ~Tracker() {
+        SPDLOG_INFO("~Tracker");
+    }
+};
+
+int format_as(const Tracker& tracker) { // NOLINT(performance-unnecessary-value-param)
+    return tracker.val;
+}
+
+Tracker get_value() {
+    return Tracker(42);
+}
+
+asio::awaitable<void> capture_lambda() {
+    auto value = get_value();
+    asio::awaitable<void> co;
+    {
+        auto lambda = [value]() -> asio::awaitable<void> {
+            co_await asio::this_coro::executor;
+            SPDLOG_INFO("the value={}", value);
+            // "value" have already been destroyed
+        };
+        co = lambda();
+    } // the lambda closure object has now gone out of scope
+    SPDLOG_INFO("about to co_await");
+    co_await std::move(co);
+}
+
 } // namespace
 
 asio::awaitable<void> run_custom_async();
@@ -38,6 +69,8 @@ int main() {
     // asio::co_spawn(io_ctx, launcher(), asio::detached);
     // asio::co_spawn(io_ctx, timer(), asio::detached);
     // asio::co_spawn(io_ctx, run_custom_async(), asio::detached);
+    // asio::co_spawn(io_ctx, capture_lambda(), asio::detached);
+
     io_ctx.run();
     return 0;
 }
